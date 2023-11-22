0604eed7cbb07a835bc592f082030d9e89b43fbd
Quynh Nguyen <quynhnguyen.swe@gmail.com>
2023-11-22 17:58:58 +0100

    Config SQLdb and config plugin ExceltoJson

--- cypress.config.js
+++ cypress.config.js
@@ -7,13 +7,41 @@ const {
 } = require('@badeball/cypress-cucumber-preprocessor/browserify')
 const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild')
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const sqlServer = require('cypress-sql-server');
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
 
 async function setupNodeEvents(on, config) {
     // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
     await addCucumberPreprocessorPlugin(on, config)
 
-    on('file:preprocessor', preprocessor(config))
+    config.db = {
+        userName: "quynhng01",
+        password: "Potato0201",
+        server: "quynhngautomation.database.windows.net",
+        options: {
+            database: "quynhngcypress",
+            encrypt: true,
+            rowCollectionOnRequestCompletion : true
+        }
+    }
+    on('file:preprocessor', preprocessor(config));
 
+    tasks = sqlServer.loadDBPlugin(config.db);
+    on('task', tasks);
+
+    on('task',
+    {
+        ExceltoJsonConverter(filePath)
+        {
+            const result = excelToJson({
+            source: fs.readFileSync(filePath) // fs.readFileSync return a Buffer
+            });
+        return result;
+        }
+    })
+
+   
     // on(
     //     'file:preprocessor',
     //     createBundler({
@@ -28,6 +56,7 @@ async function setupNodeEvents(on, config) {
 module.exports = defineConfig({
     projectId: 'yh67ny',
     defaultCommandTimeout: 6000,
+    downloadsFolder: 'cypress/downloads',
     env: {
         url: 'https://rahulshettyacademy.com',
     },
